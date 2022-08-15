f512922d1acd274d3c51e6f1c6b696517c1f121b
Andrew Watson <andrew.watson@solana.com>
2022-08-15 12:14:21 -0700

    Create sample Android app

---
+++ example/app/src/main/java/com/solanamobile/cutekittens/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.solanamobile.cutekittens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.solanamobile.cutekittens.ui.theme.CuteKittensTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CuteKittensTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CuteKittensTheme {
+        Greeting("Android")
+    }
+}
