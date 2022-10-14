6e8ae7df1dd4c5334ba778bf431aedc339b83177
jon wong <j@jnwng.com>
2022-10-13 23:13:33 -0400

    Removing `lib`

--- packages/cli/lib/cjs/helloWorld.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
-function helloWorld() {
-    return 'Hello, world!';
-}
-exports.helloWorld = helloWorld;
-//# sourceMappingURL=helloWorld.js.map
--- packages/cli/lib/esm/helloWorld.js
+++
@@ -1,4 +0,0 @@
-export function helloWorld() {
-    return 'Hello, world!';
-}
-//# sourceMappingURL=helloWorld.js.map
