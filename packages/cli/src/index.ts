9802ffe9030dd7418c72a442a196d3afc66bc399
sdlaver <103003665+sdlaver@users.noreply.github.com>
2022-12-16 11:26:07 -0800

    Bump version of dapp-store CLI tools to 0.1.3 (#96)
    * Bump version of dapp-store CLI tools to 0.1.3
    
    * Bump dapp-store-publishing-tools version in pnpm-lock.yaml

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -37,7 +37,7 @@ function resolveBuildToolsPath(buildToolsPath: string | undefined) {
 async function main() {
   program
     .name("dapp-store")
-    .version("0.1.2")
+    .version("0.1.3")
     .description("CLI to assist with publishing to the Saga Dapp Store");
 
   const createCommand = program
