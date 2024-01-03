d4f3c50753f1f367dd8472b429fcf99fe0b89ab3
sdlaver <103003665+sdlaver@users.noreply.github.com>
2024-01-02 16:35:51 -0800

    Point directly to ardrive gateway (#242)
    * Mitigate 504 errors by pointing at AR host directly
    
    * Bump version to 0.6.1

--- packages/cli/src/CliUtils.ts
+++ packages/cli/src/CliUtils.ts
@@ -18,7 +18,7 @@ import { awsStorage } from "@metaplex-foundation/js-plugin-aws";
 import { S3StorageManager } from "./config/index.js";
 
 export class Constants {
-  static CLI_VERSION = "0.6.0";
+  static CLI_VERSION = "0.6.1";
   static CONFIG_FILE_NAME = "config.yaml";
   static DEFAULT_RPC_DEVNET = "https://api.devnet.solana.com";
 
@@ -168,7 +168,7 @@ export const getMetaplexInstance = (
         providerUrl: Constants.DEFAULT_RPC_DEVNET,
       })
       : new BundlrStorageDriver(metaplex, {
-        address: "https://up.arweave.net",
+        address: "https://turbo.ardrive.io",
       });
 
     metaplex.storage().setDriver(bundlrStorageDriver);
