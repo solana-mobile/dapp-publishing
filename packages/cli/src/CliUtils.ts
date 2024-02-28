d0b4574f0434943476d8cb929eec8cffb17cd363
Ankur Jain <ankur.jain@solana.com>
2024-02-28 14:50:45 -0800

    Bump cli version to 0.7.0

--- packages/cli/src/CliUtils.ts
+++ packages/cli/src/CliUtils.ts
@@ -18,7 +18,7 @@ import { awsStorage } from "@metaplex-foundation/js-plugin-aws";
 import { S3StorageManager } from "./config/index.js";
 
 export class Constants {
-  static CLI_VERSION = "0.6.1";
+  static CLI_VERSION = "0.7.0";
   static CONFIG_FILE_NAME = "config.yaml";
   static DEFAULT_RPC_DEVNET = "https://api.devnet.solana.com";
 
