3e518d58e953c7e6e1dc9f3235fa75093a14e886
oliveeyay <olivier.goutay@gmail.com>
2025-01-30 19:54:15 +0000

    Restoring CLI tools version for this PR

--- packages/cli/src/CliUtils.ts
+++ packages/cli/src/CliUtils.ts
@@ -19,7 +19,7 @@ import { awsStorage } from "@metaplex-foundation/js-plugin-aws";
 import { S3StorageManager } from "./config/index.js";
 
 export class Constants {
-  static CLI_VERSION = "0.10.0";
+  static CLI_VERSION = "0.9.5";
   static CONFIG_FILE_NAME = "config.yaml";
   static DEFAULT_RPC_DEVNET = "https://api.devnet.solana.com";
   static DEFAULT_PRIORITY_FEE = 500000;
