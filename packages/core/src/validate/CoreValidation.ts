f1f1c8ca74243d478c5a20d7074ef4d4030400d6
Ankur Jain <ankur.jain@solana.com>
2025-07-21 14:50:54 -0700

    Cleanup old code.

--- packages/core/src/validate/CoreValidation.ts
+++ packages/core/src/validate/CoreValidation.ts
@@ -3,11 +3,9 @@ import Ajv from "ajv";
 import type {
   AppMetadata,
   MetaplexFileReleaseJsonMetadata,
-  PublisherMetadata,
 } from "../types.js";
 
 import { readFile } from 'fs/promises';
-const publisherSchema = JSON.parse((await readFile(new URL("../schemas/publisherJsonMetadata.json", import.meta.url))).toString());
 const appSchema = JSON.parse((await readFile(new URL("../schemas/appJsonMetadata.json", import.meta.url))).toString());
 const releaseSchema = JSON.parse((await readFile(new URL("../schemas/releaseJsonMetadata.json", import.meta.url))).toString());
 import { isMetaplexFile } from "@metaplex-foundation/js";
@@ -17,23 +15,6 @@ export const metaplexFileReplacer = (k: any, v: any) => {
     return "https://temp-asset-path";
   }
   return v;
-};
-
-export const validatePublisher = (publisherJson: PublisherMetadata) => {
-  const jsonToValidate = { ...publisherJson };
-  if (typeof jsonToValidate.image !== "string") {
-    jsonToValidate.image = jsonToValidate.image?.fileName;
-  }
-
-  const ajv = new Ajv({ strictTuples: false });
-  const validate = ajv.compile(publisherSchema);
-
-  const valid = validate(jsonToValidate);
-  if (!valid) {
-    console.error(validate.errors);
-    throw new Error("Publisher JSON not valid");
-  }
-  return valid;
 };
 
 export const validateApp = (appJson: AppMetadata) => {
